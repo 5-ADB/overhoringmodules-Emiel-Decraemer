d7dc17d9507654fdcebf8968807a284801c511a1
emieldecraemer <emiel.decraemer@student.tsaam.be>
2025-06-02 11:27:07 +0200

    tweede push

---
+++ huisdier.js
@@ -0,0 +1,11 @@
+function Huisdier(dier, naam, leeftijd) {
+    this.dier = dier,
+    this.naam = naam,
+    this.leeftijd = leeftijd,
+    function print(this) {
+        `mijn ${this.dier} ${this.naam} is ${this.leeftijd} jaar oud`
+    }
+    
+}
+
+module.exports = (Huisdier)
